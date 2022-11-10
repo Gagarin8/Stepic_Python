587a9435c525c810ce592fe72827e1140f49a064
Gagarin8 <114659576+Gagarin8@users.noreply.github.com>
2022-11-10 10:39:54 +0200

    Update secure_password_generator.py

--- 15_4/secure_password_generator.py
+++ 15_4/secure_password_generator.py
@@ -4,3 +4,11 @@ lowercase_letters = 'abcdefghijklmnopqrstuvwxyz'
 uppercase_letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 punctuation = '!#$%&*+-=?@^_'
 char = ''
+
+number_of_password = input('Укажите количество требуемых паролей для генерации:')
+password_lenght = input('Укажите длину пароля:')
+digits_on = input('Включить в пароль цифры 0123456789?(y/n):')
+ABC_on = input('Включить в пароль прописные буквы ABCDEFGHIJKLMNOPQRSTUVWXYZ?(y/n):')
+abc_on = input('Включить в пароль строчные буквы abcdefghijklmnopqrstuvwxyz?(y/n):')
+punct_on = input('Включить в пароль символы !#$%&*+-=?@^_?(y/n):')
+ambiguous_char = input('Исключить неодночначные символы il1Lo0O?(y/n)')
