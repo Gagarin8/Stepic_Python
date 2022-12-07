9784afcfa1daea1a47ca34ed9e6c4e713d492e98
Gagarin8 <114659576+Gagarin8@users.noreply.github.com>
2022-12-07 11:55:16 +0200

    Update secure_password_generator.py

--- 15_4/secure_password_generator.py
+++ 15_4/secure_password_generator.py
@@ -5,10 +5,33 @@ uppercase_letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 punctuation = '!#$%&*+-=?@^_'
 char = ''
 
-number_of_password = input('Укажите количество требуемых паролей для генерации:')
-password_lenght = input('Укажите длину пароля:')
+number_of_password = int(input('Укажите количество требуемых паролей для генерации:'))
+password_lenght = int(input('Укажите длину пароля:'))
 digits_on = input('Включить в пароль цифры 0123456789?(y/n):')
 ABC_on = input('Включить в пароль прописные буквы ABCDEFGHIJKLMNOPQRSTUVWXYZ?(y/n):')
 abc_on = input('Включить в пароль строчные буквы abcdefghijklmnopqrstuvwxyz?(y/n):')
 punct_on = input('Включить в пароль символы !#$%&*+-=?@^_?(y/n):')
-ambiguous_char = input('Исключить неодночначные символы il1Lo0O?(y/n)')
+ambiguous_char = input('Исключить неодночначные символы il1Lo0O?(y/n):')
+
+if digits_on.lower() == 'y':
+    char += digits
+if ABC_on.lower() == 'y':
+    char += uppercase_letters
+if abc_on.lower() == 'y':
+    char += lowercase_letters
+if punct_on.lower() == 'y':
+    char += punctuation
+if ambiguous_char.lower() == 'y':
+    for c in 'il1Lo0O':
+        char = char.replace(c, '')
+
+
+def generate_password(password_lenght, char):
+    password = ''
+    for j in range(password_lenght):
+        password += random.choice(char)
+    return password
+
+
+for _ in range(number_of_password):
+    print(generate_password(password_lenght, char))
